8dfe823b2af92d519f30bfa813d447e938494917
razzienyt <timkamotycena@gmail.com>
2025-05-02 01:16:33 +0800

    ProSeek

--- mobile/src/main/java/com/example/proseekservices/MainActivityProOP.java
+++ mobile/src/main/java/com/example/proseekservices/MainActivityProOP.java
@@ -24,57 +24,6 @@ public class MainActivityProOP extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        LinearLayout messageemily_01 = findViewById(R.id.mess_emily_01);
-        LinearLayout messagejohnduskin = findViewById(R.id.mess_johnduskin);
-        LinearLayout messagealexa99 = findViewById(R.id.mess_alexa99);
-        LinearLayout messagemikee_02 = findViewById(R.id.mess_mikee_02);
-        LinearLayout messagedavee = findViewById(R.id.mess_davee);
-        LinearLayout messagenanayluz = findViewById(R.id.mess_nanayluz);
-        LinearLayout messagema_santos = findViewById(R.id.mess_ma_santos);
-
-        messageemily_01.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "emily_01");
-            startActivity(intent);
-        });
-
-        messagejohnduskin.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "johnduskin");
-            startActivity(intent);
-        });
-
-        messagealexa99.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "alexa99");
-            startActivity(intent);
-        });
-
-        messagemikee_02.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "mikee_02");
-            startActivity(intent);
-        });
-
-        messagedavee.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "davee");
-            startActivity(intent);
-        });
-
-        messagenanayluz.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "nanayluz");
-            startActivity(intent);
-        });
-
-        messagema_santos.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivityProOP.this, MainActivityConversation.class);
-            intent.putExtra("name", "ma_santos");
-            startActivity(intent);
-        });
-
     }
 
 }
